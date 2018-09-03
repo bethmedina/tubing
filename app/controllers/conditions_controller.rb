31e1062c728f26f936a09b2e616063cbdb44f7d2
Beth Medina <medina.beth@gmail.com>
2018-09-03 14:57:34 -0600

    remove commented code

--- app/controllers/conditions_controller.rb
+++ app/controllers/conditions_controller.rb
@@ -2,8 +2,4 @@ class ConditionsController < ApplicationController
   def index
     @conditions = ConditionsService.analyze_conditions
   end
-
-  # def get_conditions
-  #   @conditions = ConditionsService.analyze_conditions
-  # end
 end
